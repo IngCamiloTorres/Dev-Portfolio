739ed9b25c108dcbb02daa05425914eeddb870de
torresing <ingsiscamilotorres@gmail.com>
2025-08-27 00:59:30 -0500

    Primer commit: importación del proyecto

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                // Optional: remove the class to re-trigger the animation every time it's scrolled to
+                // entry.target.classList.remove('show');
+            }
+        });
+    }, {
+        threshold: 0.1 // Trigger when 10% of the element is visible
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach(el => observer.observe(el));
+
+});
